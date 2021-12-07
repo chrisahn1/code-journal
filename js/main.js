2b1f630a7cdb4e43f1a5f1e98aa4b4b30d71c158
chrisahn90@yahoo.com <chrisahn90@yahoo.com>
2021-12-06 17:00:34 -0800

    Task 6 create entry main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 /* global data */
 /* exported data */
 
+var results = document.querySelector('#entry-form');
+var inputs = document.querySelector('#entry-form').elements;
+results.addEventListener('submit', submitValues);
+
+var pic = document.querySelector('#entry-form img');
 var photo = document.querySelector('#entry-form input[name=\'photoUrl\']');
 var picUpdate = document.querySelector('img');
 
@@ -8,4 +13,22 @@ photo.addEventListener('input', addPhoto);
 
 function addPhoto(event) {
   picUpdate.src = event.target.value;
+}
+
+function submitValues(event) {
+  event.preventDefault();
+  var obj = {};
+  obj.title = inputs[0].value;
+  obj.photoUrl = inputs[1].value;
+  obj.notes = inputs[2].value;
+
+  obj.entryID = data.nextEntryId;
+
+  data.nextEntryId += 1;
+
+  data.entries.push(obj);
+
+  pic.src = 'images/placeholder-image-square.jpg';
+  document.querySelector('#entry-form').reset();
+
 }
