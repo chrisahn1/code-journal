94d69eabfaf4cf1b4d3975bda000b8c7d6ff56e1
chrisahn90@yahoo.com <chrisahn90@yahoo.com>
2021-12-06 16:18:13 -0800

    Task 5 create entry main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 /* global data */
 /* exported data */
+
+var photo = document.querySelector('#entry-form input[name=\'photoUrl\']');
+var picUpdate = document.querySelector('img');
+
+photo.addEventListener('input', addPhoto);
+
+function addPhoto(event) {
+  picUpdate.src = event.target.value;
+}
