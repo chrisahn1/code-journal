d9252ea08e6da52021d1f3b8a6ce9fa37a281270
chrisahn90@yahoo.com <chrisahn90@yahoo.com>
2021-12-14 19:58:21 -0800

    task 5 view entry

--- js/main.js
+++ js/main.js
@@ -1,2 +1,26 @@
 /* global data */
 /* exported data */
+
+function renderEntries() {
+  var ul = document.getElementById('list');
+  var li = document.createElement('li');
+  var row = document.createElement('div');
+  var img = document.createElement('img');
+  var info = document.createElement('div');
+  var title = document.createElement('h3');
+  var text = document.createElement('p');
+
+  row.className = 'row';
+  title.textContent = 'Marth';
+  text.textContent = 'Victory';
+  info.appendChild(title);
+  info.appendChild(text);
+
+  img.src = 'https://ssb.wiki.gallery/images/thumb/e/e9/Marth_SSBU.png/1200px-Marth_SSBU.png';
+  row.appendChild(img);
+  row.appendChild(info);
+  li.appendChild(row);
+  ul.appendChild(li);
+}
+
+renderEntries();
