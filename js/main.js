16e5e2f042bd8a7ad116c218eaf83110c0520f8d
chrisahn90@yahoo.com <chrisahn90@yahoo.com>
2021-12-16 18:39:06 -0800

    tasks 8-9

--- js/main.js
+++ js/main.js
@@ -1,6 +1,61 @@
 /* global data */
 /* exported data */
+// SWITCHING PAGES
+var buttonEntry = document.querySelector('.view-button');
+var buttonNew = document.querySelector('.new');
 
+var enterElement = document.getElementById('test1');
+var viewElement = document.getElementById('test2');
+
+buttonEntry.addEventListener('click', viewClick);
+buttonNew.addEventListener('click', entryClick);
+
+function viewClick(event) {
+  enterElement.className = 'view hidden';
+  viewElement.className = 'view';
+}
+
+function entryClick(event) {
+  enterElement.className = 'view';
+  viewElement.className = 'view hidden';
+}
+
+// CREATE ENTRY
+var results = document.querySelector('#entry-form');
+var inputs = document.querySelector('#entry-form').elements;
+results.addEventListener('submit', submitValues);
+
+var pic = document.querySelector('#entry-form img');
+var photo = document.querySelector('#entry-form input[name=\'photoUrl\']');
+var picUpdate = document.querySelector('img');
+
+photo.addEventListener('input', addPhoto);
+
+function addPhoto(event) {
+  picUpdate.src = event.target.value;
+}
+
+function submitValues(event) {
+  event.preventDefault();
+  var obj = {};
+  obj.title = inputs[0].value;
+  obj.photoUrl = inputs[1].value;
+  obj.notes = inputs[2].value;
+
+  obj.entryID = data.nextEntryId;
+
+  data.nextEntryId += 1;
+
+  data.entries.unshift(obj);
+
+  pic.src = 'images/placeholder-image-square.jpg';
+  document.querySelector('#entry-form').reset();
+
+  enterElement.className = 'view hidden';
+  viewElement.className = 'view';
+}
+
+// VIEW ENTRY
 function renderEntries() {
   var ul = document.getElementById('list');
   var li = document.createElement('li');
@@ -16,6 +71,7 @@ function renderEntries() {
   info.appendChild(title);
   info.appendChild(text);
 
+  img.className = 'photo';
   img.src = 'https://ssb.wiki.gallery/images/thumb/e/e9/Marth_SSBU.png/1200px-Marth_SSBU.png';
   row.appendChild(img);
   row.appendChild(info);
@@ -24,3 +80,7 @@ function renderEntries() {
 }
 
 renderEntries();
+
+// window.addEventListener('DOMContentLoaded', (event) => {
+//   console.log('DOM fully loaded and parsed');
+// });
